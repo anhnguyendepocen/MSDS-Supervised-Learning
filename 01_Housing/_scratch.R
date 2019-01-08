8200034c882bfe41f48e8724ce00dbad339fbe5e
Brandon Moretz <bmoretz@ionicsolutions.net>
2019-01-08 08:59:34 -0500

    RA, ch2
    RA, ch2

--- 01_Housing/_scratch.R
+++ 01_Housing/_scratch.R
@@ -1,26 +1,45 @@
 library(data.table)
 library(ggplot2)
 
-path.work <- "E:/GitHub/MSDS-RegressionAnalysis/data"
-path.home <- "D:/Projects/MSDS-RegressionAnalysis/data"
+lp.w <- "E:/GitHub/MSDS-RegressionAnalysis"
+lp.h <- "D:/Projects/MSDS-RegressionAnalysis"
 
-if (file.exists(path.home)) {
-  setwd(path.home)
-} else {
-  setwd(path.work)
+if (file.exists(lp.w)) {
+  base.dir <- lp.w
+} else if (file.exists(lp.h)) {
+  base.dir <- lp.h
 }
 
-repairs <- read.csv("ComputerRepair.txt", sep = "\t")
+data.path <- paste0(base.dir, "/data/")
 
-x_hat <- mean(repairs$Minutes)
-y_hat <- mean(repairs$Units)
+# simple replacement for read.csv that returns a data.table.
+loadDataFile <- function(file_name, sep = ',') {
+  data.raw <- fread(paste0(data.path, file_name),
+    header = TRUE, sep = sep, stringsAsFactors = FALSE, na.strings = c("NA", ""))
 
-sum((repairs$Units - y_hat) * (repairs$Minutes - x_hat))
+  data <- setNames(data.raw, tools::toTitleCase(tolower(names(data.raw))))
 
-sum((repairs$Minutes - x_hat)) ^ 2
+  return(data)
+}
+
+repairs <- loadDataFile("ComputerRepair.txt", sep = '\t')
+
+y <- repairs$Units
+x <- repairs$Minutes
+
+x_hat <- mean(x)
+y_hat <- mean(y)
+
+b1 <- sum((y - y_hat) * (x - x_hat)) / sum((y - y_hat) * (y - y_hat))
+b0 <- mean(x) - beta1 * mean(y)
+plot(x, y)
+abline(b0, b1)
+
 
 fit <- lm(repairs$Units ~ repairs$Minutes)
 summary(fit)
+coeff = coefficients(fit)
+eq = paste0("y = ", round(coeff[2], 1), "*x ", round(coeff[1], 1))
 
-plot(repairs)
+plot(x, y)
 abline(fit)
