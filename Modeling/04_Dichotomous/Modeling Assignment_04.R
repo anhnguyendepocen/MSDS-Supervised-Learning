809f9ec92374795901ea20585140db39ce6f15c6
Brandon Moretz <bmoretz@ionicsolutions.net>
2019-08-26 13:56:01 -0400

    Modeling #4
    Modeling #4

--- Modeling/04_Dichotomous/Modeling Assignment_04.R
+++ Modeling/04_Dichotomous/Modeling Assignment_04.R
@@ -40,6 +40,79 @@ theme_update(plot.title = element_text(hjust = 0.5),
              legend.position = "top", legend.title = element_blank())
 
 data.wine <- as.data.table(read.csv(file = "wine.csv", head = TRUE, sep = ","))
+data.wine$`ï..INDEX` <- NULL # remove index column
 
 # EDA
+
+skim(data.wine)
+
+ggpairs(data.wine)
+
+ggplot(data.wine, aes(Cases, fill = ..count..)) +
+  geom_histogram()
+
+ggplot(data.wine, aes(Alcohol, fill = ..count..)) +
+  geom_histogram()
+
+ggplot(data.wine, aes(Chlorides, fill = ..count..)) +
+  geom_histogram()
+
+ggplot(data.wine, aes(ResidualSugar, fill = ..count..)) +
+  geom_histogram()
+
+ggplot(data.wine, aes(Cases, fill = ..count..)) +
+  geom_histogram()
+
+ggplot(data.wine, aes(STARS, fill = ..count..)) +
+  geom_histogram()
+
+# Continuous
+
+ggplot(data.wine, aes(VolatileAcidity, fill = ..count..)) +
+  geom_histogram()
+
+# Continuous
+ggplot(data.wine, aes(FreeSulfurDioxide, fill = ..count..)) +
+  geom_histogram()
+
+# Normal Discrete
+ggplot(data.wine, aes(LabelAppeal, fill = ..count..)) +
+  geom_histogram()
+
+# Poisson Discrete
+ggplot(data.wine, aes(AcidIndex, fill = ..count..)) +
+  geom_histogram()
+
+#####################################################################
+### Variable Transformations
+
+# Transform Label Appeal, standardize label appeal to 1-5
+
+data.wine$LabelAppeal <- data.wine$LabelAppeal + 3
+
+ggplot(data.wine, aes(LabelAppeal, fill = ..count..)) +
+  geom_histogram()
+
+#####################################################################
+### Response, STARS
+
+data.stars <- data.wine[!is.na(STARS)]
+
+skim(data.stars)
+
+#####################################################################
+### Purchase Decision
+
+# Logistic Regression
+ggplot(data.wine, aes(Purchase, fill = ..count..)) +
+  geom_histogram()
+
+
+#####################################################################
+### Number of Cases Sold
+
+# Zero Inflated Binomial (ZIP)
+
+ggplot(data.wine, aes(Cases, fill = ..count..)) +
+  geom_histogram()
 
